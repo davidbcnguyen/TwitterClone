421c33f014ce55ddf62fdbcc87d540fd2e4fb11b
davidbcnguyen <101545438+davidbcnguyen@users.noreply.github.com>
2022-05-17 15:48:05 -0700

    Set up nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,53 +1,63 @@
 import React from 'react';
-import { Counter } from './Components/Counter';
+import { BrowserRouter, Routes } from 'react-router-dom';
+import NavBar from './Components/NavBar';
 
 function App() {
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <Counter />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <span>
+  //         <span>Learn </span>
+  //         <a
+  //           className="App-link"
+  //           href="https://reactjs.org/"
+  //           target="_blank"
+  //           rel="noopener noreferrer"
+  //         >
+  //           React
+  //         </a>
+  //         <span>, </span>
+  //         <a
+  //           className="App-link"
+  //           href="https://redux.js.org/"
+  //           target="_blank"
+  //           rel="noopener noreferrer"
+  //         >
+  //           Redux
+  //         </a>
+  //         <span>, </span>
+  //         <a
+  //           className="App-link"
+  //           href="https://redux-toolkit.js.org/"
+  //           target="_blank"
+  //           rel="noopener noreferrer"
+  //         >
+  //           Redux Toolkit
+  //         </a>
+  //         ,<span> and </span>
+  //         <a
+  //           className="App-link"
+  //           href="https://react-redux.js.org/"
+  //           target="_blank"
+  //           rel="noopener noreferrer"
+  //         >
+  //           React Redux
+  //         </a>
+  //       </span>
+  //     </header>
+  //   </div>
+  // );
   return (
     <div className="App">
-      <header className="App-header">
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <NavBar></NavBar>
+      <BrowserRouter>
+        <Routes>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/Components/NavBar.js
@@ -0,0 +1,26 @@
+import { Container, Form, FormControl, Nav, Navbar } from "react-bootstrap";
+
+export default function NavBar() {
+    return (
+        <Navbar expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand>Twitter Clone</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Form className="me-auto">
+                        <FormControl type="search" placeholder="Search users" className="me-2" aria-label="Search"/>
+                    </Form>
+                    <Nav>
+                        <Nav.Link>Home</Nav.Link>
+                        <Nav.Link>Feed</Nav.Link>
+                        <Nav.Link>Profile</Nav.Link>
+                        <Nav.Link>Logout</Nav.Link>
+                    </Nav>
+                    <Nav className="d-flex">
+                        <Nav.Link href="https://github.com/davidbcnguyen/TwitterClone">Source Code</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
