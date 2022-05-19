7fd2d7fb038d0b01e5206a82937b0d57944e7d0a
davidbcnguyen <101545438+davidbcnguyen@users.noreply.github.com>
2022-05-18 18:58:02 -0700

    Fix styling of posters list

--- frontend/src/Components/Posters.js
+++ frontend/src/Components/Posters.js
@@ -1,11 +1,19 @@
-import { ListGroup } from "react-bootstrap";
+import { Col, Container, ListGroup, Row } from "react-bootstrap";
 import Poster from "./Poster";
 
 export default function Posters({ posters=[] }) {
     const posterCards = posters.map(p => <ListGroup.Item key={p.username} as={Poster} username={p.username} createdAt={p.createdAt} />)
     return (
-        <ListGroup>
-            {posterCards}
-        </ListGroup>
+        <Container>
+            <Row>
+                <Col sm={4}>
+                    <ListGroup>
+                        {posterCards}
+                    </ListGroup>
+                </Col>
+                <Col sm={8}>
+                </Col>
+            </Row>
+        </Container>
     );
 }
